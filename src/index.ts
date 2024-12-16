882e3a92560c23d3c7037ff2b4465aa7bd23bab2
Johan Kivi <johan.kivi@zocom.se>
2024-12-16 13:49:31 +0100

    hooks added

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,22 @@ const fastify = Fastify({
   logger: true,
 });
 
-fastify.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.info(`🚀 Server is up n running on ${address}.`);
+fastify.addHook("onResponse", async (request, reply) => {
+  // Some async code run after response, eg. logs, traces, metrics
 });
+
+fastify.addHook("onError", async (request, reply, error) => {
+  // Useful for custom error logging
+  // You should not use this hook to update the error
+});
+
+fastify.listen(
+  { port: process.env.PORT ? parseInt(process.env.PORT) : 3000 },
+  (err, address) => {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.info(`🚀 Server is up n running on ${address}.`);
+  }
+);
